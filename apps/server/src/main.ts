dd73e473d54b2d0d18902feb33bdf5abdfe83700
Kashif-Kamran <kashifkamran.aalvi@gmail.com>
2025-02-15 01:41:57 +0500

    Updated the location of global response interceptor

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TypedConfigService } from './configuration/configuration.module';
-import { GlobalResponseInterceptor } from './common/global-response.interceptor';
+import { GlobalResponseInterceptor } from './common/interceptors/global-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
