cd6fa8e3cbf65177e0c7d4250d45137de7d17fd1
Kashif-Kamran <kashifkamran.aalvi@gmail.com>
2025-02-16 05:41:01 +0500

    Updated the endpoint

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,2 +1,7 @@
-export { UserModel } from './models/user.model';
-export { AnimalModel } from './models/animal.model';
+export { UserModel, UserDocument, UserSchema, User } from './models/user.model';
+export {
+  AnimalModel,
+  AnimalDocument,
+  AnimalSchema,
+  Animal,
+} from './models/animal.model';
