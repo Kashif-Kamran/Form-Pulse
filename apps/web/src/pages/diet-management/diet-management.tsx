a80cff8bc5ea7842a5a00e74791e93f54dc7ea7c
dev <dev@leads-test.local>
2025-02-27 03:10:59 +0500

    top bar created

--- apps/web/src/pages/diet-management/diet-management.tsx
+++ apps/web/src/pages/diet-management/diet-management.tsx
@@ -1,8 +1,26 @@
-
+import InputField from "@/components/custom-ui/form-feilds/input-field"
+import { SearchIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { CreateAnimalModel } from "../animals/components/create-animal-model"
 
 const DietManagement = () => {
   return (
-    <div>Diet Management ksjndkjcndc</div>
+    <div className="flex items-center gap-4">
+        <InputField
+          className="rounded-full bg-white pr-0 border-0 shadow-md"
+          leftIcon={<SearchIcon />}
+          placeholder="Search Animal..."
+          rightIcon={
+            <Button
+              size={"icon"}
+              className="rounded-r-full py-6 px-12 [&_svg]:size-none"
+            >
+              Search
+            </Button>
+          }
+        />
+        <CreateAnimalModel />
+      </div>
   )
 }
 
