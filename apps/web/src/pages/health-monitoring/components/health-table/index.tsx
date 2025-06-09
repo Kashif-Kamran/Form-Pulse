90f1d22975fe40f64a7ac819b96af490af163834
Kashif-Kamran <kashifkamran.aalvi@gmail.com>
2025-06-09 14:37:49 +0500

    refactor: Remove unused state and functions from HealthMonitoringTable component

--- apps/web/src/pages/health-monitoring/components/health-table/index.tsx
+++ apps/web/src/pages/health-monitoring/components/health-table/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Table,
@@ -12,91 +11,24 @@ import { Button } from "@/components/ui/button";
 import { Trash2Icon } from "lucide-react";
 import { PopulatedAnimalHealthRecord } from "@repo/shared";
 
-interface Animal {
-  id: number;
-  name: string;
-  age: string;
-  breed: string;
-  weight: string;
-  status: string;
-  vaccine: string;
-}
-
 export interface HealthMonitoringTableProps {
   results: PopulatedAnimalHealthRecord[];
 }
 
 const HealthMonitoringTable = ({ results }: HealthMonitoringTableProps) => {
-  const [animals, setAnimals] = useState<Animal[]>([
-    {
-      id: 1,
-      name: "Camel 1",
-      age: "7 Yrs",
-      breed: "Breed",
-      weight: "90 kgs",
-      status: "Add",
-      vaccine: "Anti-MERS",
-    },
-    {
-      id: 2,
-      name: "Camel 2",
-      age: "7 Yrs",
-      breed: "Breed",
-      weight: "92 kgs",
-      status: "2 Remaining",
-      vaccine: "Anti-MERS",
-    },
-    {
-      id: 3,
-      name: "Camel 3",
-      age: "5 Yrs",
-      breed: "Breed",
-      weight: "88 kgs",
-      status: "Vaccinated",
-      vaccine: "Anti-MERS",
-    },
-    {
-      id: 4,
-      name: "Horse 1",
-      age: "7 Yrs",
-      breed: "Breed",
-      weight: "82 kgs",
-      status: "2 Remaining",
-      vaccine: "Anti-MERS",
-    },
-    {
-      id: 5,
-      name: "Horse 2",
-      age: "8 Yrs",
-      breed: "Breed",
-      weight: "91 kgs",
-      status: "3 Remaining",
-      vaccine: "Anti-MERS",
-    },
-    {
-      id: 6,
-      name: "Horse 3",
-      age: "8 Yrs",
-      breed: "Breed",
-      weight: "91 kgs",
-      status: "3 Remaining",
-      vaccine: "Anti-MERS",
-    },
-  ]);
+  // const handleDelete = (id: number) => {
+  //   setAnimals(animals.filter((animal) => animal.id !== id));
+  // };
 
-  const handleDelete = (id: number) => {
-    setAnimals(animals.filter((animal) => animal.id !== id));
-  };
+  // const getStatusColor = (status: string) => {
+  //   if (status === "Add") return "text-green-500";
+  //   if (status === "Vaccinated") return "text-blue-500";
+  //   return "text-red-500"; // For "Remaining" numbers
+  // };
 
-  const getStatusColor = (status: string) => {
-    if (status === "Add") return "text-green-500";
-    if (status === "Vaccinated") return "text-blue-500";
-    return "text-red-500"; // For "Remaining" numbers
-  };
-
-  const rowClick = (id: number) => {
-    console.log("Row clicked:", id);
-  };
+  // const rowClick = (id: number) => {
+  //   console.log("Row clicked:", id);
+  // };
 
   return (
     <div className="flex flex-col overflow-hidden bg-white rounded-xl">
@@ -114,7 +46,7 @@ const HealthMonitoringTable = ({ results }: HealthMonitoringTableProps) => {
             </TableRow>
           </TableHeader>
           <TableBody className="flex-1">
-            {animals.length === 0 && (
+            {results.length === 0 && (
               <TableRow>
                 <TableCell className="p-4" colSpan={7}>
                   No Animals Found
